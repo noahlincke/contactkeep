1f5a0eee3cc97dd8ff9441a328f61c7b34686b80
Noah Lincke <noah@lincke.org>
2019-06-23 20:14:35 -0700

    Finished API and created basic front-end

---
+++ client/src/context/contact/ContactState.js
@@ -0,0 +1,65 @@
+import React, { useReducer } from "react";
+import uuid from "uuid";
+import ContactContext from "./contactContext";
+import contactReducer from "./contactReducer";
+import {
+  ADD_CONTACT,
+  DELETE_CONTACT,
+  SET_CURRENT,
+  CLEAR_CURRENT,
+  UPDATE_CONTACT,
+  FILTER_CONTACTS,
+  CLEAR_FILTER
+} from "../types";
+
+const ContactState = props => {
+  const initialState = {
+    contacts: [
+      {
+        id: 1,
+        name: "Jill Johnson",
+        email: "jill@gmail.com",
+        phone: "111-111-1111",
+        type: "personal"
+      },
+      {
+        id: 2,
+        name: "Bob Johnson",
+        email: "bob@gmail.com",
+        phone: "211-111-1111",
+        type: "personal"
+      },
+      {
+        id: 3,
+        name: "Harry Johnson",
+        email: "harry@gmail.com",
+        phone: "311-111-1111",
+        type: "professional"
+      }
+    ]
+  };
+
+  const [state, dispatch] = useReducer(contactReducer, initialState);
+
+  //Add Contact
+
+  //Delete Contact
+
+  //Set Current
+
+  //Clear Current
+
+  //Update Contact
+
+  //Filter Contacts
+
+  //Clear Filter
+
+  return (
+    <ContactContext.Provider value={{ contacts: state.contacts }}>
+      {props.children}
+    </ContactContext.Provider>
+  );
+};
+
+export default ContactState;
