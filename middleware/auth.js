05b3ebc39324b7e9d550fe03bf98f093334d808b
Noah Lincke <noah@lincke.org>
2019-06-26 23:11:46 -0700

    kjh

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,21 +1,19 @@
-const jwt = require('jsonwebtoken');
-const config = require('config');
+const jwt = require("jsonwebtoken");
 
 module.exports = function(req, res, next) {
-  // Get token from header
-  const token = req.header('x-auth-token');
+  //Get token from header\
+  const token = req.header("x-auth-token");
 
-  // Check if not token
+  //Check if not token
   if (!token) {
-    return res.status(401).json({ msg: 'No token, authorization denied' });
+    return res.status(401).json({ msg: "No token, authorization denied" });
   }
 
   try {
-    const decoded = jwt.verify(token, config.get('jwtSecret'));
-
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
     req.user = decoded.user;
     next();
   } catch (err) {
-    res.status(401).json({ msg: 'Token is not valid' });
+    res.status(401).json({ msg: "Token is not valid" });
   }
 };
