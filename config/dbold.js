7484e55e219e47278588a1a573ceebf3f77e152e
Noah Lincke <noah@lincke.org>
2019-06-26 22:36:35 -0700

    fixing db

--- config/dbold.js
+++ config/dbold.js
@@ -1,6 +1,6 @@
-const mongoose = require("mongoose");
-const config = require("config");
-const db = process.env.MONGO_URI;
+const mongoose = require('mongoose');
+const config = require('config');
+const db = config.get('mongoURI');
 
 const connectDB = async () => {
   try {
@@ -9,7 +9,8 @@ const connectDB = async () => {
       useCreateIndex: true,
       useFindAndModify: false
     });
-    console.log("MongoDB Connected...");
+
+    console.log('MongoDB Connected...');
   } catch (err) {
     console.error(err.message);
     process.exit(1);
