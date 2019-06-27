d1c5dacdb41539672c4bd7b476462dc8dc5bacad
Noah Lincke <noah@lincke.org>
2019-06-26 23:21:23 -0700

    fix

--- routes/users.js
+++ routes/users.js
@@ -1,25 +1,25 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const bcrypt = require("bcryptjs");
-const jwt = require("jsonwebtoken");
-const secret = process.env.JWT_SECRET;
-const { check, validationResult } = require("express-validator");
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
+const { check, validationResult } = require('express-validator/check');
 
-const User = require("../models/User");
+const User = require('../models/User');
 
-// @route   POST api/users
-// @desc    Register a user
-// @access  Public
+// @route     POST api/users
+// @desc      Regiter a user
+// @access    Public
 router.post(
-  "/",
+  '/',
   [
-    check("name", "Please add name")
+    check('name', 'Please add name')
       .not()
       .isEmpty(),
-    check("email", "Please include a valid email").isEmail(),
+    check('email', 'Please include a valid email').isEmail(),
     check(
-      "password",
-      "Please enter a password with 6 or more characters"
+      'password',
+      'Please enter a password with 6 or more characters'
     ).isLength({ min: 6 })
   ],
   async (req, res) => {
@@ -32,40 +32,43 @@ router.post(
 
     try {
       let user = await User.findOne({ email });
+
       if (user) {
-        return res.status(400).json({ msg: "User already exists" });
-      } else {
-        user = new User({
-          name,
-          email,
-          password
-        });
-
-        const salt = await bcrypt.genSalt(10);
-        user.password = await bcrypt.hash(password, salt);
-        await user.save();
-
-        const payload = {
-          user: {
-            id: user.id
-          }
-        };
-
-        jwt.sign(
-          payload,
-          secret,
-          {
-            expiresIn: 360000
-          },
-          (err, token) => {
-            if (err) throw err;
-            res.json({ token });
-          }
-        );
+        return res.status(400).json({ msg: 'User already exists' });
       }
+
+      user = new User({
+        name,
+        email,
+        password
+      });
+
+      const salt = await bcrypt.genSalt(10);
+
+      user.password = await bcrypt.hash(password, salt);
+
+      await user.save();
+
+      const payload = {
+        user: {
+          id: user.id
+        }
+      };
+
+      jwt.sign(
+        payload,
+        config.get('jwtSecret'),
+        {
+          expiresIn: 360000
+        },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
     } catch (err) {
       console.error(err.message);
-      res.status(500).send("Server error");
+      res.status(500).send('Server Error');
     }
   }
 );
